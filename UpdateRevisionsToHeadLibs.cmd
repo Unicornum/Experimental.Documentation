d6ed5afd1455cb9f9d085baa5da5be35fb4cbbcc
CTAPOBEP <CTAPOBEP@a365ddb6-76be-9f42-b586-fd62945542ec>
2015-12-13 21:51:20 +0000

    Базовая структура папок/файлов.
    git-svn-id: file:///E/Repository/Example.Documentation/trunk@2 a365ddb6-76be-9f42-b586-fd62945542ec

---
+++ UpdateRevisionsToHeadLibs.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+set SvnExternals=.\svn.externals
+
+set UrlToLibs="https://svn/svn/Libs/trunk"
+
+set PathToAlicornInclude=./Alicorn/include
+set UrlToAlicornInclude=https://svn/svn/Libs/trunk/Solution/Solution/Alicorn/include
+
+set PathToExternals=./Externals
+set UrlToExternals=https://svn/svn/Libs/trunk/Solution/Solution/Externals
+
+rem ====== HEAD ревизи€ дл€ Libs ======
+
+for /F "Delims=" %%I In (
+	'svn info --show-item=revision %UrlToLibs% --revision HEAD'
+) do set LibsHeadRevision=%%~I
+
+rem ====== ‘ормирование списка папок дл€ обновлени€ ревизии ======
+
+del %SvnExternals%
+
+echo %PathToAlicornInclude% -r %LibsHeadRevision% %UrlToAlicornInclude% >> %SvnExternals%
+echo %PathToExternals% -r %LibsHeadRevision% %UrlToExternals% >> %SvnExternals%
+
+rem ====== ”становка свойств/обновление ревизии ======
+
+svn propset svn:externals -F%SvnExternals% ./Externals
+
+del %SvnExternals%
+
+pause
