2cb60d79fd6f7c3d0081fb8477a42ce521ac2ec0
CTAPOBEP <CTAPOBEP@a365ddb6-76be-9f42-b586-fd62945542ec>
2015-12-17 13:54:12 +0000

    Логика обновления номера ревизии перенесена в проект.
    git-svn-id: file:///E/Repository/Example.Documentation/trunk@15 a365ddb6-76be-9f42-b586-fd62945542ec

---
+++ Solution/Version.hpp
@@ -0,0 +1,43 @@
+
+#pragma once
+
+/**
+* \file
+*  Файл версии решения.
+*/
+
+#define VERSION 0
+
+#define SUBVERSION 0
+
+#define SUBSUBVERSION 0
+
+// Устанавливается автоматически
+#define REVISION_SOLUTION 14
+
+// Устанавливается автоматически
+#define REVISION_LIBS 1240
+
+// Устанавливается автоматически
+#define __ARCHITECTURE__ x86.32
+
+// Устанавливается автоматически
+#define TIME "16:45"
+
+// Устанавливается автоматически
+#define DAY 17
+
+// Устанавливается автоматически
+#define MONTH Декабрь
+
+// Устанавливается автоматически
+#define YEAR 2015
+
+// Текущий год устанавливается автоматически
+#define __COPYRIGHT_STR__ "© UnicornumVerum 2015 (unicornum.verum@gmail.ru)"
+
+#define __PRODUCT_NAME__ "Продукт: Props"
+
+#define __FILE_DESCRIPTION_STR__ ""
+
+#define __FILE_NAME__ ""
