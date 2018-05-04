2bf1f64b978320c23c3564236ec3d0a905883d79
CTAPOBEP <CTAPOBEP@a365ddb6-76be-9f42-b586-fd62945542ec>
2018-05-04 08:43:33 +0000

    [Utilities.Documentation] Обновление ревизии ссылок.
    git-svn-id: file:///E/Repository/Example.Documentation/master/Utilities.Documentation@345 a365ddb6-76be-9f42-b586-fd62945542ec

--- Solution/Version.hpp
+++ Solution/Version.hpp
@@ -13,19 +13,19 @@
 #define SUBSUBVERSION 0
 
 // Устанавливается автоматически
-#define REVISION_SOLUTION 241
+#define REVISION_SOLUTION 344
 
 // Устанавливается автоматически
 #define __PLATFORM__ x64
 
 // Устанавливается автоматически
-#define TIME "20:58"
+#define TIME "11:42"
 
 // Устанавливается автоматически
-#define DAY 22
+#define DAY 04
 
 // Устанавливается автоматически
-#define MONTH Апрель
+#define MONTH Май
 
 // Устанавливается автоматически
 #define YEAR 2018
